385842d1760862b74c6419bba4b5aba0874250bb
Facco99 <facchin810@gmail.com>
2020-03-19 16:00:38 +0100

    utilizzo ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  componentToSho: number = 2;
   
   title='Titolo';
 
   clickAppComponent(value: string){
     console.log("Cliccato da secondo component"+value)
+  }
+
+  showComponent(){
+    if(this.componentToSho === 3)
+      this.componentToSho=1;
+    else
+      this.componentToSho++;
   }
 }
