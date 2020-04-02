03e39a4de930c0e72fbeb545d7f8ed812ba0db4d
Facco99 <facchin810@gmail.com>
2020-04-02 15:21:44 +0200

    uso reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -8,22 +8,57 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class ReactiveFormComponent implements OnInit {
 
+  get nameControl(): FormControl{
+    return this.heroForm.get('name') as FormControl;
+  }
+  
+  get powerControl(): FormControl{
+    return this.heroForm.get('power') as FormControl;
+  }
   powers:string[] = ['superforza','supervelocita','volare'];
+
+  heroList:{name:string, power:string}[]=[];
 
   heroForm:FormGroup
 
   constructor(private fb:FormBuilder) { 
-    this.heroForm=fb.group({
+    this.heroForm=this.fb.group({
       name: ['',Validators.compose([Validators.required, Validators.minLength(3)])],
       power: ['',Validators.required],
-      street: '',
-      city: '',
-      state: '',
-      zip: ''
+      address : this.fb.group({
+        street: '',
+        city: '',
+        state: '',
+        zip: ['', Validators.compose([Validators.minLength(5),Validators.maxLength(5)])]
+      })
     });
   }
 
   ngOnInit(): void {
+  }
+
+  setDefault(){
+    // this.heroForm.setValue({
+    //   name:'Goku'
+    // });
+    this.heroForm.patchValue({
+      name:'Goku'
+    });
+  }
+
+  clearHero(){
+    this.heroForm.reset({
+      name:'Gohan'
+    });
+  }
+
+  addHero(){
+    this.heroList.push({
+      name: this.nameControl.value,
+      power: this.powerControl.value,
+    });
+    this.clearHero();
+
   }
 
 }
